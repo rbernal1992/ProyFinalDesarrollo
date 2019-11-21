75a4fe7e0bbf70e29718fe6e16278c43fbf41a15
rbernal1992 <54275705+rbernal1992@users.noreply.github.com>
2019-11-20 23:21:36 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/usersDB";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://rbernal1992:honera2020@cluster0-t4okh.mongodb.net/SiasaDB?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 8080;
 
